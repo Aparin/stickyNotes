ed71ff40ba03ee1d8ef6db782f56a8fd719f7cb4
Апарин Александр <mir-uspeha@mail.ru>
2018-08-27 09:56:49 +0300

    output headline and body at sticker's editing

--- src/editSticker.js
+++ src/editSticker.js
@@ -1,11 +1,16 @@
+import { makeSticker } from './makeSticker';
+
 export default function editSticker(id) {
     let sticker = document.getElementById(id);
+    let head = makeSticker(id).head;
+    let body = makeSticker(id).body;
     sticker.addEventListener('dblclick', function() {
         if (sticker.className === 'newSticker') {
-            alert('newSticker');
             sticker.classList = 'editSticker';
+            sticker.innerHTML = (`<input type="text"size="38" name="head" style="border: 0px; font-size:14px; width:290px; margin: 5px;" value="${head}" /><br />	
+            <textarea style="width: 290px; height:265px;  margin: 3px; border: 0px;" name="textArea">${body}</textarea>`);
+
         } else {
-            alert('editSticker');
             sticker.className = 'newSticker';
         }
 
