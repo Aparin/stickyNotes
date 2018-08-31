5e10b987eceed5c8b50132da93d7cb07209f5977
Апарин Александр <mir-uspeha@mail.ru>
2018-08-31 22:33:52 +0300

    upgraded design of panel's buttons

--- src/panel.js
+++ src/panel.js
@@ -14,12 +14,9 @@ if (!id) { id = 0; }
 function panel() {
     let panelContents = `
     <div id="panel" class="panel">
-        <div id="plus" class="symbol" title="Добавить стикер">+</div>
-        <!--
-        <div id="sort" class="symbol" style="font-size: 44px;" title="Упорядочить">≚</div>
-        -->
-        <div id="save" class="symbol" style="font-size: 35px;" title="Сохранить всё">💾</div>       
-        <div id="clear" class="symbol" title="Удалить всё"><img src="img/urn.png"></div>
+        <div id="plus" class="symbol" title="Добавить стикер"><img src="img/plus_32.png"></div>
+        <div id="save" class="symbol" title="Сохранить всё"><img src="img/save_32.png"></div>       
+        <div id="clear" class="symbol" title="Удалить всё"><img src="img/clear_32.png"></div>
     </div>`;
 
     document.getElementById('stickyNotes').insertAdjacentHTML('beforeend', panelContents);
