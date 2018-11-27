6e24289e0c4630262d9e74f98e9d651a957023a8
Апарин Александр <mir-uspeha@mail.ru>
2018-11-27 23:17:32 +0300

    changed design of stickers

--- src/view/example.js
+++ src/view/example.js
@@ -10,8 +10,8 @@ export default function example() {
     makeSticker('Learn React', 'On middle level', 'newSticker');
     makeSticker('Learn Node.JS', 'And develop multi-user services', 'newSticker');
     makeSticker('To find a cool job', 'With good salary', 'newSticker');
-    makeSticker('ВДНХ на карте', 'Посетить выставку промышленного оборудования (пищевая пром-ть; станки для бахил и т.п.)', 'map', 'Пр-т Мира, 119, Москва');
     makeSticker('Питер, Фонтанка', '', 'map', 'Питер, Фонтанка');
+    makeSticker('ВДНХ на карте', 'Посетить выставку промышленного оборудования (пищевая пром-ть; станки для бахил и т.п.)', 'map', 'Пр-т Мира, 119, Москва');
 
     const ex = document.getElementById('example');
     ex.remove();
