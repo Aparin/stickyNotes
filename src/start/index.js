b81385b07f43dbb08595592088f30f3589905f37
Апарин Александр <mir-uspeha@mail.ru>
2018-12-09 18:23:30 +0300

    made menu

--- src/start/index.js
+++ src/start/index.js
@@ -1,10 +1,5 @@
 import './bootstrap.css';
 import './../view/main/main.css';
-//import 'bootstrap';
-
-//import './bootstrap';
-// import './bootstrap.bundle';
-//require('bootstrap');
 
 import menu from './../view/menu/menu';
 import load from './../control/load';
